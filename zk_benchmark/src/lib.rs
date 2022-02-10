82ce703057252c1eed16fcc96bed87d02130c88c
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-02-10 14:22:13 -0300

    refactoring into lib.rs. Got too crowded.

--- zk_benchmark/src/lib.rs
+++ zk_benchmark/src/lib.rs
@@ -1,4 +1,12 @@
-use std::{env, error::Error, fmt, fs};
+use ethers::{prelude::*, providers};
+use std::sync::Arc;
+use std::time::Duration;
+use std::{error::Error, fmt, fs};
+// use zk_benchmark::{run, Config};
+//
+mod assembly_sol;
+
+type HttpProvider = providers::Provider<providers::Http>;
 
 #[derive(Debug, Clone)]
 struct AbiError(String);
@@ -10,9 +18,9 @@ impl fmt::Display for AbiError {
 }
 impl Error for AbiError {}
 
-pub fn get_abi(config: &Config) -> Result<String, Box<dyn Error>> {
-    if let Ok(abi) = fs::read_to_string(&config.filename) {
-        return Ok(abi);
+pub fn create_typesafe_abi(config: &Config) -> Result<(), Box<dyn Error>> {
+    if fs::read_to_string(&config.filename).is_ok() {
+        return Ok(());
     }
     // ethers::contract::Abigen::new()
     ethers::contract::Abigen::new("AssemblySol", &config.contract_abi)?
@@ -24,7 +32,87 @@ pub fn get_abi(config: &Config) -> Result<String, Box<dyn Error>> {
     //     Err(e) => Err(e),
     // }
     // let abi = fs::read_to_string(&config.filename);
-    Ok(abi)
+    Ok(())
+}
+
+type SigningKey = ethers::core::k256::ecdsa::SigningKey;
+
+type ClientMiddleware = ethers::middleware::NonceManagerMiddleware<
+    ethers::middleware::SignerMiddleware<
+        ethers::providers::Provider<ethers::providers::Http>,
+        ethers::signers::Wallet<SigningKey>,
+    >,
+>;
+
+#[derive(Debug, Clone)]
+struct MyWallet {
+    pvtkey: Wallet<SigningKey>,
+    pubkey: H160,
+}
+
+impl MyWallet {
+    fn new(pvtkey: &str) -> MyWallet {
+        let pvtkey = pvtkey.parse::<LocalWallet>().unwrap();
+
+        let pubkey = pvtkey.address();
+
+        MyWallet { pvtkey, pubkey }
+    }
+}
+
+// type MyClient = Arc<ClientMiddleware>;
+fn setup_client(provider_addr: &str, wallet: MyWallet) -> Arc<ClientMiddleware> {
+    let provider = HttpProvider::try_from(provider_addr)
+        .unwrap()
+        .interval(Duration::from_millis(6u64));
+    println!("provider: {:?}", provider);
+
+    let client = SignerMiddleware::new(provider, wallet.pvtkey);
+    let client = NonceManagerMiddleware::new(client, wallet.pubkey);
+    let client = Arc::new(client);
+
+    client
+}
+
+pub async fn run() -> bool {
+    // let pvtkey = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7"
+    //     .parse::<LocalWallet>()
+    //     .unwrap();
+    //
+    // let pubkey = pvtkey.address();
+    // println!("pubkey: {}", pubkey.to_string());
+    let pvtkey = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7";
+    let wallet = MyWallet::new(pvtkey);
+
+    println!("wallet: {:?}", wallet);
+    // let dest_chain_id = dest_client.get_chainid().await?;
+    // let dest_contract = FixedDepositAnchorContract::new(
+    //     linked_anchor.address,
+    //     dest_client.clone(),
+    // );
+    // let provider = Provider::<Http>::try_from("http://localhost:8545").unwrap();
+    // let provider = provider.interval(Duration::from_millis(1000));
+    // let client = SignerMiddleware::new(provider, wallet);
+    // let client = NonceManagerMiddleware::new(client, address);
+    // let client = Arc::new(client);
+    let client = setup_client("http://127.0.0.1:9545", wallet);
+
+    println!("client: {:?}", client);
+    let address = "0x1d412664e5B1c9518995Cf411e8C2F4CC929D5C2"
+        .parse::<H160>()
+        .unwrap();
+    let contract = assembly_sol::AssemblySol::new(address, client.clone());
+
+    // let contract = ethers::contract::Contract::new(address, abi, client);
+    println!();
+    let val = contract.hello_world().call().await.unwrap();
+    println!("contract: {:?}", val);
+
+    // let init_value: String = contract
+    // .method::<_, String>("getValue", ())?
+    // .call()
+    // .await?;
+    true
 }
 
 pub struct Config {
@@ -33,12 +121,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(mut args: env::Args) -> Result<Self, &'static str> {
-        args.next();
-
-        let contract_abi = args.next().ok_or("Didn't get abi file")?;
-        let filename = args.next().ok_or("Didn't get rs file")?;
-
+    pub fn new(contract_abi: String, filename: String) -> Result<Self, &'static str> {
         Ok(Config {
             contract_abi,
             filename,
--- zk_benchmark/src/main.rs
+++ zk_benchmark/src/main.rs
@@ -1,65 +1,53 @@
+use clap::{App, Arg};
 use ethers::{prelude::*, providers};
-use std::error::Error;
-use std::sync::Arc;
+use std::process;
 use std::time::Duration;
-use std::{env, process};
 // use zk_benchmark::{run, Config};
 //
 mod assembly_sol;
 
-type HttpProvider = providers::Provider<providers::Http>;
 #[tokio::main]
 async fn main() {
-    let pvtkey = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7"
-        .parse::<LocalWallet>()
-        .unwrap();
+    let matches = App::new("ZK Benchmark")
+        .version("0.0.1")
+        .author("Semar Augusto <semaraugusto@gmail.com>")
+        .about("Allows for gas and proving time benchmarking for zk contracts")
+        .arg(
+            Arg::new("command")
+                .short('c')
+                .long("command")
+                .takes_value(true)
+                .help("Build/Run"),
+        )
+        .arg(
+            Arg::new("abi")
+                .long("abi")
+                .takes_value(true)
+                .help("Path to contract json abi"),
+        )
+        .arg(
+            Arg::new("output_path")
+                .short('o')
+                .long("output-path")
+                .takes_value(true)
+                .help("Path to rust typesafe contract abi"),
+        )
+        .get_matches();
 
-    let pubkey = pvtkey.address();
-    println!("pubkey: {}", pubkey.to_string());
-
-    let config = zk_benchmark::Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    let abi = zk_benchmark::get_abi(&config).map_err(|e| -> Result<String, Box<dyn Error>> {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
-    });
-
-    // let dest_chain_id = dest_client.get_chainid().await?;
-    // let dest_contract = FixedDepositAnchorContract::new(
-    //     linked_anchor.address,
-    //     dest_client.clone(),
-    // );
-
-    let provider = HttpProvider::try_from("http://127.0.0.1:9545")
-        .unwrap()
-        .interval(Duration::from_millis(6u64));
-    println!("provider: {:?}", provider);
-
-    let client = SignerMiddleware::new(provider, pvtkey);
-    let client = NonceManagerMiddleware::new(client, pubkey);
-    let client = Arc::new(client);
-    // let provider = Provider::<Http>::try_from("http://localhost:8545").unwrap();
-    // let provider = provider.interval(Duration::from_millis(1000));
-    // let client = SignerMiddleware::new(provider, wallet);
-    // let client = NonceManagerMiddleware::new(client, address);
-    // let client = Arc::new(client);
-
-    println!("client: {:?}", client);
-    let address = "0x1d412664e5B1c9518995Cf411e8C2F4CC929D5C2"
-        .parse::<H160>()
-        .unwrap();
-    let contract = assembly_sol::AssemblySol::new(address, client.clone());
-
-    // let contract = ethers::contract::Contract::new(address, abi, client);
-    println!();
-    let val = contract.hello_world().call().await.unwrap();
-    println!("contract: {:?}", val);
-
-    // let init_value: String = contract
-    // .method::<_, String>("getValue", ())?
-    // .call()
-    // .await?;
+    let command = matches.value_of("command").unwrap_or("run");
+    let config = zk_benchmark::Config::new(
+        matches.value_of("abi").unwrap().to_string(),
+        matches.value_of("output_path").unwrap().to_string(),
+    )
+    .unwrap();
+    if command == "build" {
+        if let Err(e) = zk_benchmark::create_typesafe_abi(&config) {
+            eprintln!("Building error error: {}", e);
+            process::exit(1);
+        };
+    } else {
+        while zk_benchmark::run().await {
+            std::thread::sleep_ms(5000);
+        }
+    }
 }
